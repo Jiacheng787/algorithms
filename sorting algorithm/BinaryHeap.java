27f3d2d86722555acd48763862c40558c1d40eb4
Jiacheng Dong <43509711+Jiacheng787@users.noreply.github.com>
2021-03-06 09:23:55 +0800

    initial commit

---
+++ sorting algorithm/BinaryHeap.java
@@ -0,0 +1,67 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class BinaryHeap {
+    // 在构造器中使用的变量必须单独声明，否则访问不到
+    private int[] data;
+    private int size;
+    public BinaryHeap(int[] arr) {
+        // 构造器名称必须和类名一致
+        // 构造器和实例方法中，加不加this都无所谓
+        this.data = arr;
+        this.size = arr.length;
+    }
+    public void maxHeapify(int i) {
+        int max = i;
+
+        if (i >= this.size) return;
+
+        int left = i * 2 + 1;
+        int right = i * 2 + 2;
+
+        if ((left < this.size) && (this.data[left] > this.data[max])) {
+            max = left;
+        }
+
+        if ((right < this.size) && (this.data[right] > this.data[max])) {
+            max = right;
+        }
+
+        if (max == i) return;
+
+        this.swap(this.data, i, max);
+
+        this.maxHeapify(max);
+    }
+    public void rebuildHeap() {
+        int L = this.size / 2; // int类型自动向下取整
+        for(int i = L - 1; i >= 0; i--){
+            this.maxHeapify(i);
+        }
+    }
+    public void sort() {
+        for(int i = this.size - 1; i > 0; i--){
+            this.swap(this.data, 0, i);
+            this.size--; // 将交换后的元素分离出堆
+            this.maxHeapify(0);
+        }
+        this.size = this.data.length;
+    }
+    public int[] getHeap() {
+        return this.data;
+    }
+    public void swap(int[] A, int i, int j) {
+        int t = A[i];
+        A[i] = A[j];
+        A[j] = t;
+    }
+    public static void main(String[] args) {
+        int[] arr = {15, 2, 8, 12, 5, 2, 3, 4, 7};
+        com.company.BinaryHeap binaryHeap = new com.company.BinaryHeap(arr);
+        binaryHeap.rebuildHeap();
+        binaryHeap.sort();
+        int[] res = binaryHeap.getHeap();
+        System.out.println(Arrays.toString(res));
+    }
+}
